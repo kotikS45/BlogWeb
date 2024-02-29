e0f49e3c0d366cf5b82f497b623b83ad626eebc5
unknown <sashakotik450697@gmail.com>
2024-02-29 20:25:02 +0200

    Add categories

---
+++ BlogWebApi/BlogWebApi/Data/AppEFContext.cs
@@ -0,0 +1,21 @@
+﻿using BlogWebApi.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace BlogWebApi.Data
+{
+    public class AppEFContext : DbContext
+    {
+        public AppEFContext(DbContextOptions<AppEFContext> options) : base(options) { }
+
+        public DbSet<CategoryEntity> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ BlogWebApi/BlogWebApi/Data/Entities/CategoryEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogWebApi.Data.Entities
+{
+    [Table("tblCategories")]
+    public class CategoryEntity : BaseEntity<int>
+    {
+        [Required, StringLength(255)]
+        public string Name { get; set; }
+        [StringLength(200)]
+        public string UrlSlug { get; set; }
+        [Required, StringLength(1000)]
+        public string Description { get; set; }
+    }
+}
